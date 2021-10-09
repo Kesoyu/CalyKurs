3ae66de0d3f4784722ec32722f2c6afb4f5599b4
3a1Kesoyu <janusztm4@gmail.com>
2021-10-09 12:55:23 +0200

    Cala Pierwsza lekcja kod

---
+++ KursPythonDay1/main.py
@@ -0,0 +1,24 @@
+import demos.demo01_print as dp#as pozwala na skrocenie sciezki odwolania do importu
+from demos.demo02_types import Basic_types #import globalny
+#importujemy tylko wybrana funkcje
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#TabNine/Kite programy Atom
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    from demos.demo02_types import Basic_types, Typy_Kolekcyjne, Typy_Binarne  # import lokalny
+    # print_hi('Bartosz')
+    # print(dp.DemoPrint.__doc__)  # __doc__ wyciaganie dokumentacji i jeje wypisywanie
+    # dp.DemoPrint()
+    # Basic_types()
+    # Typy_Kolekcyjne()
+    Typy_Binarne()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
